03b2e961b9c4f3c097febf45e44b2293537f9129
Rian Dultra (PC) <rian.albert.dultra@gmail.com>
2026-06-15 01:38:20 -0300

    refactor: extract common items and sulfuras logic finalizing decompression

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -49,37 +49,21 @@ class GildedRose(object):
             if item.sell_in < 5 and item.quality < 50:
                 item.quality += 1
 
-    def old_update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+    def _update_common_item(self, item):
+        # RF-01: Diminui o prazo de venda
+        item.sell_in -= 1
 
+        # RF-02 & RF-04: Degradação padrão respeitando o piso de 0
+        if item.quality > 0:
+            item.quality -= 1
+            
+            # RF-03: Se já venceu, degrada em dobro (-2 no total)
+            if item.sell_in < 0 and item.quality > 0:
+                item.quality -= 1
 
+    def _update_sulfuras(self, item):
+        # RF-11.1, RF-11.2, RF-11.3: Itens lendários não mudam nada
+        pass
 
 class Item:
     def __init__(self, name, sell_in, quality):
